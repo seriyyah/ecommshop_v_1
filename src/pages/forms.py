d5616393521cb47329e9cedf59de02ba1fafbc2e
seriyyah <46221195+seriyyah@users.noreply.github.com>
2019-11-03 08:20:19 +0200

    none

---
+++ src/pages/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django_countries.fields import CountryField
+from  django_countries.widgets import CountrySelectWidget
+PAYMENT_CHOICES = (
+	('S', 'Stripe'),
+	('P', 'Paypal')
+	)
+
+class CheckoutForm(forms.Form):
+	shipping_address = forms.CharField(required=False)
+	shipping_address2 = forms.CharField(required=False)
+	shipping_country = CountryField(blank_label='(select country)').formfield(
+		required=False, 
+		widget=CountrySelectWidget(attrs={
+		'class': 'custom-select d-block w-100',
+		}))
+	shipping_zip = forms.CharField(required=False)
+
+	set_default_shipping = forms.BooleanField(required=False)
+	use_default_shipping = forms.BooleanField(required=False)
+	
+	payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+class CouponForm(forms.Form):
+	code = forms.CharField(widget=forms.TextInput(attrs={
+			'class': 'form-control',
+			'placeholder': 'Promo code',
+			'arial-label': 'Recipient\'s username',
+			'aria-describedby': 'basic-addon2'
+		}))
+
+class RefundForm(forms.Form):
+	ref_code = forms.CharField()
+	massage = forms.CharField(widget=forms.Textarea(attrs={
+		'rows': 4
+		}))
+	email = forms.EmailField()
+
+class PaymentForm(forms.Form):
+	stripeToken = forms.CharField(required=False)
+	save = forms.BooleanField(required=False)
+	use_default = forms.BooleanField(required=False)
